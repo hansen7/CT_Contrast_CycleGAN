f76ccd36d484fbf63b6442f897f1102900612245
hansen7 <hc.wang96@gmail.com>
2020-09-09 21:57:25 +0100

    add test image generation scripts

--- pytorch-CycleGAN-and-pix2pix-master/test_2nocontrast.sh
+++ pytorch-CycleGAN-and-pix2pix-master/test_2nocontrast.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-python train.py \
+python test.py \
     --batch_size 9 \
     --gpu_ids 0,1,2,3 \
     --name contrast2no \
-    --dataroot datasets/contrast2no \
+    --dataroot datasets/contrast2no/testA \
     --input_nc 1 \
     --output_nc 1 \
     --load_size 512 \
     --crop_size 512 \
     --display_winsize 512 \
-    --display_id 0;
+    --display_id 0 \
+    --model test \
+    --no_dropout;
 
 # https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/issues/24
+# train/testA: contrast images
+# train/testB: no contrast images
