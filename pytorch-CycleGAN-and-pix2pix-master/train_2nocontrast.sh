f69980c0260fb640d5de70b31b9871d3cdcdd65a
roserland <1393745326@qq.com>
2020-09-09 01:41:25 +0000

    change no display

--- pytorch-CycleGAN-and-pix2pix-master/train_2nocontrast.sh
+++ pytorch-CycleGAN-and-pix2pix-master/train_2nocontrast.sh
@@ -1,10 +1,13 @@
 python train.py \
-    --batch_size 1 \
-    --gpu_ids 0 \
+    --batch_size 9 \
+    --gpu_ids 0,1,2,3 \
     --name contrast2no \
     --dataroot datasets/contrast2no \
     --input_nc 1 \
     --output_nc 1 \
     --load_size 512 \
     --crop_size 512 \
-    --display_winsize 512 ;
+    --display_winsize 512 \
+    --display_id 0;
+
+# https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/issues/24
